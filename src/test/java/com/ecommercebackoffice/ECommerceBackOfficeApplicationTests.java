f92e64ca6abb984c612ab0db2ca20a8d3dc6c47a
maschemy <121471220+maschemy@users.noreply.github.com>
2026-04-23 16:32:01 +0900

    프로젝트 시작

---
+++ src/test/java/com/ecommercebackoffice/ECommerceBackOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommercebackoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
