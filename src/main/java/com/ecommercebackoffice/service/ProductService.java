b0d16b7b3f654c50cda0bdfde68b8d0717bf6ca9
sighingpotato <jot0202@naver.com>
2026-04-24 11:46:24 +0900

    feat : Service 파일 생성

--- src/main/java/com/ecommercebackoffice/service/ProductService.java
+++ src/main/java/com/ecommercebackoffice/service/ProductService.java
@@ -1,4 +1,11 @@
 package com.ecommercebackoffice.service;
 
+import com.ecommercebackoffice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepository productRepository;
 }
