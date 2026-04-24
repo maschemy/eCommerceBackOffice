5ca99ab8bed4090391ab13fd41f92ef55fd80bc2
sighingpotato <jot0202@naver.com>
2026-04-24 14:44:46 +0900

    fix : 페이징 응답 DTO 제네릭 처리

--- src/main/java/com/ecommercebackoffice/product/dto/PageResponseDto.java
+++ src/main/java/com/ecommercebackoffice/product/dto/PageResponseDto.java
@@ -1,4 +1,31 @@
 package com.ecommercebackoffice.product.dto;
 
-public class PageResponseDto {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+// <ProductListResponseDto> -> <T>로 제네릭 처리
+@Getter
+@RequiredArgsConstructor
+public class PageResponseDto<T> {
+    // 목록 조회시 페이징 정보와 데이터 반환
+    private final List<T> content;
+    private final int page;
+    private final int size;
+    private final long totalElements;
+    private final int totalPages;
+    private final boolean first;
+    private final boolean last;
+
+    public PageResponseDto(Page<T> page) {
+        this.content = page.getContent();
+        this.page = page.getNumber() + 1;
+        this.size = page.getSize();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = getTotalPages();
+        this.first = page.isFirst();
+        this.last = page.isLast();
+    }
 }
--- src/main/java/com/ecommercebackoffice/product/dto/UpdateProductRequestDto.java
+++ src/main/java/com/ecommercebackoffice/product/dto/UpdateProductRequestDto.java
@@ -1,4 +1,22 @@
 package com.ecommercebackoffice.product.dto;
 
-public class UpdateProductRequest {
+import com.ecommercebackoffice.product.entity.ProductCategory;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class UpdateProductRequestDto {
+    // 수정 가능한 필드만 포함
+    @NotBlank(message = "상품명은 필수입니다.")
+    @Size(max = 100, message = "상품명은 100자 이하 입니다.")
+    private String name;
+
+    @NotBlank(message = "카테고리명은 필수입니다.")
+    private ProductCategory category;
+
+    @NotBlank(message = "가격은 필수입니다.")
+    @Min(value = 0, message = "가격은 0 이상이어야 합니다.")
+    private Integer price;
 }
