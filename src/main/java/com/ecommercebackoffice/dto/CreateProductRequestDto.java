5c8410ad669c40e06b36254e9dd44e0d642f211d
sighingpotato <jot0202@naver.com>
2026-04-24 11:25:00 +0900

    feat : 상품 등록 요청 DTO 정의

---
+++ src/main/java/com/ecommercebackoffice/dto/CreateProductRequestDto.java
@@ -0,0 +1,30 @@
+package com.ecommercebackoffice.dto;
+
+import com.ecommercebackoffice.entity.ProductCategory;
+import com.ecommercebackoffice.entity.ProductStatus;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+// 상품 등록시 필요한 입력 값 정의
+@Getter
+public class CreateProductRequestDto {
+
+    @NotBlank(message = "상품명은 필수입니다.")
+    @Size(max = 100, message = "상품명은 100자 이하 입니다.")
+    private String name;
+
+    @NotBlank(message = "카테고리명은 필수입니다.")
+    private ProductCategory category;
+
+    @NotBlank(message = "가격은 필수입니다.")
+    @Min(value = 0, message = "재고는 0 이상이어야 합니다.")
+    private Integer stock;
+
+    @NotBlank(message = "상태는 필수입니다.")
+    private ProductStatus status;
+
+    @NotBlank(message = "관리자 ID는 필수입니다.")
+    private Long adminId;
+}
