27dabbb1471a146af593418cbf652b4b4e23c1c8
minkim11 <alsgur4772@gmail.com>
2026-04-24 11:40:33 +0900

    # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력 # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성 feat : Order Entity, Repository 추가 ################ # 본문(추가 설명)을 아랫줄에 작성 entity - Order 클래스 추가 - SoftDelete 구현 - 기본적인 틀만 일단 구현 repository 추가 ################ #Feat: 새로운 기능을 추가할 경우 #Fix: 버그를 고친 경우 #!HOTFIX: 급하게 치명적인 버그를 고쳐야하는 경우 #Refactor: 프로덕션 코드 리팩토링 #Comment: 필요한 주석 추가 및 변경 #Docs: 문서를 수정한 경우 #Rename: 파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우 #Remove: 파일을 삭제하는 작업만 수행한 경우 ################

---
+++ src/main/java/com/ecommercebackoffice/order/entity/Order.java
@@ -0,0 +1,50 @@
+package com.ecommercebackoffice.order.entity;
+
+import com.ecommercebackoffice.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+
+@SQLDelete(sql = "UPDATE orders SET deletedAt = NOW() WHERE id = ?")
+@SQLRestriction("deletedAt IS NULL")
+@Getter
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    private int quantity;
+    private int totalPrice;
+    private String status = "준비중";
+    private String cancelReason;
+    private LocalDateTime deletedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "admin_id", nullable = false)
+    private Admin admin;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    public Order(Admin admin, Customer customer, Product product,String orderNumber, int quantity, int totalPrice) {
+        this.admin = admin;
+        this.customer = customer;
+        this.product = product;
+        this.orderNumber = orderNumber;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
+}
