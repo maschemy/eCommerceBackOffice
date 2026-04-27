7914a497af3c3a6e9e6ec9a522a042aeac016a5d
maschemy <121471220+maschemy@users.noreply.github.com>
2026-04-27 17:13:57 +0900

    feat: UpdateCustomerRequestDto랑 Customer 수정 -> Customer에 있는 update함수에 name이랑 phoneNumber가 !null && ! .isBlank() 일때만 데이터를 검증해서 변경을 적용하도록 만듦

--- src/main/java/com/ecommercebackoffice/customer/dto/UpdateCustomerRequestDto.java
+++ src/main/java/com/ecommercebackoffice/customer/dto/UpdateCustomerRequestDto.java
@@ -1,17 +1,16 @@
 package com.ecommercebackoffice.customer.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class UpdateCustomerRequestDto {
-    @NotBlank
+
     private String name;
     @Email
     private String email;
-    @NotBlank
+
     @Pattern(regexp = "^010-\\d{4}-\\d{4}$")
     private String phoneNumber;
 }
