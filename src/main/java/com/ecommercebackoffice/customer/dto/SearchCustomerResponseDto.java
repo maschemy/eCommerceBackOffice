342af73488e49db691b984b3e616169bad3dfd40
maschemy <121471220+maschemy@users.noreply.github.com>
2026-04-27 16:50:45 +0900

    feat: 1. UpdateCustomerRequestDto에 어노테이션 추가 2. 고객 조회 때 modifiedAt 출력되도록 추가 3. 로그인해야 customer에 관한 기능을 사용할 수 있도록 변경

--- src/main/java/com/ecommercebackoffice/customer/dto/SearchCustomerResponseDto.java
+++ src/main/java/com/ecommercebackoffice/customer/dto/SearchCustomerResponseDto.java
@@ -13,17 +13,19 @@ public class SearchCustomerResponseDto {
     private final String phoneNumber;
     private final CustomerStatus customerStatus;
     private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
     private final Long totalOrderCount;
     private final Long totalOrderAmount;
 
     public SearchCustomerResponseDto(
-            Long id,String name, String email, String phoneNumber, CustomerStatus customerStatus, LocalDateTime createdAt, Long totalOrderCount, Long totalOrderAmount) {
+            Long id,String name, String email, String phoneNumber, CustomerStatus customerStatus, LocalDateTime createdAt,LocalDateTime modifiedAt, Long totalOrderCount, Long totalOrderAmount) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.customerStatus = customerStatus;
         this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
         this.totalOrderCount = totalOrderCount;
         this.totalOrderAmount = totalOrderAmount;
     }
