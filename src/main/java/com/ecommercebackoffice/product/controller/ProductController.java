2c7e1f40a00ea668d5af1fda424d20da9e1d4e94
베려하는마음 <102654500+sighingpotato@users.noreply.github.com>
2026-04-29 15:27:43 +0900

    Add authentication requirement to ProductController

--- src/main/java/com/ecommercebackoffice/product/controller/ProductController.java
+++ src/main/java/com/ecommercebackoffice/product/controller/ProductController.java
@@ -16,7 +16,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-
+@PreAuthorize("isAuthenticated()")
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
