5d82270216aa37eb33eb58c5313310c1b98ef217
maschemy <121471220+maschemy@users.noreply.github.com>
2026-04-29 15:00:40 +0900

    feat : Customer, Dashboard를 jwt 방식으로 로그인 할 수 있게 변경

--- src/main/java/com/ecommercebackoffice/customer/controller/CustomerController.java
+++ src/main/java/com/ecommercebackoffice/customer/controller/CustomerController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -34,7 +35,7 @@ public class CustomerController {
      */
     @GetMapping
     public ResponseEntity<Page<SearchCustomerResponseDto>> getAllCustomer(
-            @SessionAttribute(name = Const.LOGIN_ADMIN) LoginAdmin loginAdmin,
+            @AuthenticationPrincipal LoginAdmin loginAdmin,
             @RequestParam(required = false) String keyword,
             @RequestParam(required = false) CustomerStatus status,
             @RequestParam(defaultValue = "1") int page,
@@ -53,7 +54,7 @@ public class CustomerController {
      */
     @GetMapping("/{customerId}")
     public ResponseEntity<SearchCustomerResponseDto> getOneCustomer(
-            @SessionAttribute(name = Const.LOGIN_ADMIN) LoginAdmin loginAdmin,
+            @AuthenticationPrincipal LoginAdmin loginAdmin,
             @PathVariable Long customerId){
         return ResponseEntity.status(HttpStatus.OK).body(customerService.findOne(customerId));
     }
@@ -66,7 +67,7 @@ public class CustomerController {
      */
     @PatchMapping("/{customerId}")
     public ResponseEntity<UpdateCustomerResponseDto> updateCustomerInfo(
-            @SessionAttribute(name = Const.LOGIN_ADMIN) LoginAdmin loginAdmin,
+            @AuthenticationPrincipal LoginAdmin loginAdmin,
             @Valid
             @RequestBody UpdateCustomerRequestDto request,
             @PathVariable Long customerId
@@ -82,7 +83,7 @@ public class CustomerController {
      */
     @PatchMapping("/{customerId}/status")
     public ResponseEntity<UpdateCustomerStatusResponseDto> updateCustomerStatus(
-            @SessionAttribute(name = Const.LOGIN_ADMIN) LoginAdmin loginAdmin,
+            @AuthenticationPrincipal LoginAdmin loginAdmin,
             @Valid @RequestBody UpdateCustomerStatusRequestDto request,
             @PathVariable Long customerId
     ){
@@ -96,7 +97,7 @@ public class CustomerController {
      */
     @DeleteMapping("/{customerId}")
     public ResponseEntity<Void> deleteCustomerId(
-            @SessionAttribute(name = Const.LOGIN_ADMIN) LoginAdmin loginAdmin,
+            @AuthenticationPrincipal LoginAdmin loginAdmin,
             @PathVariable Long customerId){
         customerService.delete(customerId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
--- src/main/java/com/ecommercebackoffice/dashboard/controller/DashboardController.java
+++ src/main/java/com/ecommercebackoffice/dashboard/controller/DashboardController.java
@@ -8,6 +8,7 @@ import com.ecommercebackoffice.dashboard.service.DashboardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class DashboardController {
      */
     @GetMapping
     public ResponseEntity<DashboardResponseDto> getDashboard(
-            @SessionAttribute(name = Const.LOGIN_ADMIN) LoginAdmin loginAdmin
+            @AuthenticationPrincipal LoginAdmin loginAdmin
     ){
         return ResponseEntity.status(HttpStatus.OK).body(dashboardService.getDashboard());
     }
