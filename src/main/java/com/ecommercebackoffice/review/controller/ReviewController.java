b097612676c3f693a2c806dcc9a30331e713ddd6
minkim11 <alsgur4772@gmail.com>
2026-04-29 15:00:26 +0900

    refactor : 주문, 리뷰 로그인 방식 변경
    Session -> JWT 인증/인가

--- src/main/java/com/ecommercebackoffice/review/controller/ReviewController.java
+++ src/main/java/com/ecommercebackoffice/review/controller/ReviewController.java
@@ -1,7 +1,6 @@
 package com.ecommercebackoffice.review.controller;
 
 import com.ecommercebackoffice.auth.dto.LoginAdmin;
-import com.ecommercebackoffice.common.Const;
 import com.ecommercebackoffice.product.dto.PageResponseDto;
 import com.ecommercebackoffice.review.dto.ReviewDetailResponseDto;
 import com.ecommercebackoffice.review.dto.ReviewListResponseDto;
@@ -9,6 +8,8 @@ import com.ecommercebackoffice.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +19,7 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     // 리뷰 리스트 조회 API
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'OPERATION_ADMIN')")
     @GetMapping
     public ResponseEntity<PageResponseDto<ReviewListResponseDto>> getReviews(
             @RequestParam(required = false) String keyword, // 고객명, 상품명
@@ -26,7 +28,7 @@ public class ReviewController {
             @RequestParam(defaultValue = "10") int size, // 페이지당 개수
             @RequestParam(defaultValue = "createdAt") String sortBy, // 정렬 기준
             @RequestParam(defaultValue = "desc") String sortOder, // 정렬 방향
-            @SessionAttribute(name = Const.LOGIN_ADMIN) LoginAdmin loginAdmin
+            @AuthenticationPrincipal LoginAdmin loginAdmin
     ) {
         PageResponseDto<ReviewListResponseDto> result = reviewService.getReviews(
                 keyword, rating, page, size ,sortBy, sortOder);
@@ -34,20 +36,22 @@ public class ReviewController {
     }
 
     // 리뷰 상세 조회 API
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'OPERATION_ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<ReviewDetailResponseDto> getReview(
             @PathVariable Long id,
-            @SessionAttribute(name = Const.LOGIN_ADMIN) LoginAdmin loginAdmin
+            @AuthenticationPrincipal LoginAdmin loginAdmin
     ) {
         ReviewDetailResponseDto result = reviewService.getReview(id);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     // 리뷰 삭제 API
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'OPERATION_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReview(
             @PathVariable Long id,
-            @SessionAttribute(name = Const.LOGIN_ADMIN) LoginAdmin loginAdmin
+            @AuthenticationPrincipal LoginAdmin loginAdmin
     ) {
         reviewService.deleteReview(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
